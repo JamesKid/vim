f26181247bfdd47f449e3e51edafb3b1721e4269
JamesKid <406358964@qq.com>
2014-01-18 17:45:42 +0800

    add vim execute linux command line

--- use_tips.sh
+++ use_tips.sh
@@ -1,12 +1,27 @@
+#==============================================================
 #!/bin/bash 
 # Description:  this is some vim use tips here 
 # Editor:       JamesKid 
 # Email:		 406358964@qq.com 
 # Version:      1.0 
-# History:      2014-01-04 creat JamesKid
+# History:      2014-01-04_00_00 creat JamesKid
+#               2014_01_18_17_43 add exec linux command
+#==============================================================
 
 # coding
 	:set fenc=utf8  #1.transfer the file to utf-8
 
 # regular expression
 	:%s/href="[^"]\+=[^{css}]"/href="#"/g    # replace the href except xxxx.css"
+# use linux or cmd command
+	:!command
+	:!ls -l
+	:r !command		# get the content result ,and print it in the current file
+	:62,72 !sort	# sort the 62 to 72
+	:24 w ! bash    # excute the code of line 19
+	:. w !bash      # use the current file as command 
+	:21 !bash		# it will excute line 21 and replace the lline 21
+	:shell			# go to shell command line 
+	ls -l
+
+
