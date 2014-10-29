de2ec628d72e23d73ead07d80492f1cb202dc7d0
JamesKid <406358964@qq.com>
2014-10-29 17:30:25 +0800

    fix vim74 install in centos

--- note/2_install_vim74_in_centos.sh
+++ note/2_install_vim74_in_centos.sh
@@ -13,7 +13,6 @@
 	su - root
 	yum remove vim vim-enhanced vim-common vim-minimal 
 # get file and unzip
-	wget ftp://ftp.vim.org/pub/vim/unix/vim-7
 	wget ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2  
 	wget ftp://ftp.vim.org/pub/vim/extra/vim-7.2-extra.tar.gz//网上的那个链接不能用  
 	wget ftp://ftp.vim.org/pub/vim/extra/vim-7.2-lang.tar.gz
